4f7348c5b9f8511e1ecbe5a71fbe74c93dcdb22f
wangyu <wangyu@uniontech.com>
2022-12-29 11:38:16 +0800

    feat: remove redundant directories
    remove redundant directories
    
    Log: remove redundant directories

--- src/lsdevice/lsdevice.cmake
+++ src/lsdevice/lsdevice.cmake
@@ -5,8 +5,8 @@ file(GLOB OUTER_SOURCE
 
 file(GLOB OUTER_HEADER
     ${CMAKE_CURRENT_LIST_DIR}/../../src/3rdparty/*.h
-    ${CMAKE_CURRENT_LIST_DIR}/../../include/dtkdevice/lsdevice/*.h
-    ${CMAKE_CURRENT_LIST_DIR}/../../include/dtkdevice/global/*.h
+    ${CMAKE_CURRENT_LIST_DIR}/../../include/lsdevice/*.h
+    ${CMAKE_CURRENT_LIST_DIR}/../../include/global/*.h
 )
 
 set(LS_SRCS
